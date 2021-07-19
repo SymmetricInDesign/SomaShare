9ad10ad8fbea3d8bf3017a8c51395b2672dd0332
Donnie Wombough <donniewomboughjunior@gmail.com>
2021-07-18 23:55:14 -0400

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+const express = require("express");
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+const db = require('./config/keys').mongoURI;
+
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
