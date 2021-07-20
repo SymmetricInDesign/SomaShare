b1ca406bb2b26dd9316b759ceaf11176b701b960
Priyesh Shah <priyesh.shah147@gmail.com>
2021-07-20 14:44:47 -0400

    Css and post form

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import LoginFormContainer from '../components/session/login_form_container'
 import SignupFormContainer from '../components/session/signup_form_container'
-import NavBar from "../components/nav/navbar_container"
+import NavBar from "../components/nav/navbar_container";
+import CreatePostContainer from "../components/posts/create_post_container";
+import UpdatePostContainer from "../components/posts/create_post_container";
 // import MainPage from './main/main_page';
 import Splash from './splash/splash'
 import { Switch, Route } from 'react-router-dom';
+
 
 
 
@@ -15,7 +18,10 @@ const App = () => (
         <Switch>
             <AuthRoute exact path="/login" component={LoginFormContainer}/>
             <AuthRoute exact path="/signup" component={SignupFormContainer}/>
+            <ProtectedRoute exact path='/posts/new' component={CreatePostContainer}/>
+            <ProtectedRoute exact path='/posts/edit' component={UpdatePostContainer}/>
             <Route exact path="/" component={Splash} />
+            
         </Switch>
     </>
 );
---
+++ frontend/src/components/posts/post_form.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class PostForm extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = this.props.post
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.action(this.state);
+    }
+    render(){
+        return (
+            <div>
+                <h2>{this.post.formType}</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label className="post-label">Title
+                        <input type="string" 
+                        value={this.state.title} 
+                        className="post-input"/>
+                    </label>
+                    <label className="post-label">Category</label>
+                    <select name="subject" className="subject">
+                        <option value="Mathematics">Mathematics</option>
+                        <option value="Science">Science</option>
+                        <option value="English">English</option>
+                        <option value="Computers">Computers</option>
+                    </select>
+                    <label className="post-label">Description
+                        <textarea 
+                        value={this.state.title} 
+                        className="post-input"/>
+                    </label>
+                    <label className="post-label">Link
+                        <input type="string" 
+                        value={this.state.link} 
+                        className="post-input"/>
+                    </label>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default PostForm;
