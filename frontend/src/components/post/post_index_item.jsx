c771b5d6edf2781809506a250ea2c1b55c0257d2
wliu <wliu0709@gmail.com>
2021-07-20 20:16:15 -0400

    fix bug in getDate

--- frontend/src/components/post/post_index_item.jsx
+++ frontend/src/components/post/post_index_item.jsx
@@ -26,22 +26,28 @@ class PostIndexItem extends React.Component{
          let day = date.slice(8, 10)
        return (
             <div className='post-date'>
-                <p className='post-month'>{monthHead[month]}</p> <p className='friend-bill-day'>{day}</p>
+                <p className='post-month'>{monthHead[month]}</p> <p className='post-day'>{day}</p>
             </div>
         )
     }  
 
     render(){
         return(
-            <div className="post-details">
+        <li>
 
-                <a href={this.props.post.link} target="_blank" >
+            <div className="post-details">
+                <div className='post-index-item'>
+                    <span className='post-left'>
+                        {this.getDate()} <p>{this.props.post.title}</p>
+                    </span>
+                {/* <a href={this.props.post.link} target="_blank" >
                 {this.props.post.title}
-                </a>
+                </a> */}
                 <h2> {this.props.post.description}</h2>
-        
-                <p>{this.props.post.createdAt}</p>
             </div>
+            </div>
+        </li>
+        
         )
     }
 }
