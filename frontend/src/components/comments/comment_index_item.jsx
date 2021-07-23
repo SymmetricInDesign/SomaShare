9a940ec7209c4f930fe1c3456ce7c972e066713e
Yung Cai <yungcai@Yungs-MBP.fios-router.home>
2021-07-22 20:19:10 -0400

    comment form stlying

--- frontend/src/components/comments/comment_index_item.jsx
+++ frontend/src/components/comments/comment_index_item.jsx
@@ -10,8 +10,9 @@ class CommentIndexItem extends React.Component{
     render(){
         return(
             <div className='comment-index-item'>
-                <div className='comment-username'>{this.props.comment.username}:</div>
-                <div className='comment-body'>{this.props.comment.commentBody}</div>
+                <div className='comment-username'>{this.props.comment.username}:</div><br></br>
+                <div className='comment-body'>{this.props.comment.commentBody}</div><br></br>
+                <div className='comment-date'>Last updated: {this.props.comment.updatedAt.slice(0, 10)}</div>
             </div>
         )
     }
