9ac4a628f48ea63241e0cbf71a940686d2e3eda9
Donnie Wombough <donniewomboughjunior@gmail.com>
2021-08-05 15:43:37 -0400

    add post index sort, add meta tags for default image

--- frontend/src/components/search/post_search_results.jsx
+++ frontend/src/components/search/post_search_results.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import PostIndexItem from '../post/post_index_item'
 import { withRouter } from 'react-router'
+import {Link} from 'react-router-dom';  
+
 const queryString = require('query-string')
+
 class PostSearch extends React.Component{
+    // constructor(props){
+    //     super(props)
+    //     this.state = {activeButton: "newest"}
+    //     this.postsSortedByDate = [] 
+    // }
+
     componentDidMount(){
         const searchFilter = queryString.parse(this.props.location.search)
         this.props.requestPosts(searchFilter.category, searchFilter.searchText)
@@ -14,48 +23,84 @@ class PostSearch extends React.Component{
           this.props.requestPosts(searchFilter.category, searchFilter.searchText)
         }
     }
+
+    setActive(str){
+        console.log(str)
+        this.setState({activeButton: str})
+      }
+    
+    ratingComparator(post1, post2){
+        if (post1.rating > post2.rating){
+            return 1
+        }else if (post1.rating < post2.rating){
+            return -1
+        }else{
+            return 0
+        }
+    }
  
-        render() {
-            const {posts} = this.props
-          return (
+    render() {
+        let {posts} = this.props     
+        // if (!(this.postsSortedByDate.length > 0) && posts.length>0){
+        //     this.postsSortedByDate=[]
+        //     posts.forEach(post=>{
+        //         this.postsSortedByDate.push(post)
+        //     })
+        // }
         
+        // if (this.state.activeButton === "best"){
+        //     posts.sort(this.ratingComparator)
+        // }
+        
+        // if (this.state.activeButton === "newest" && this.postsSortedByDate.length > 0){
+        //     posts = this.postsSortedByDate
+        // }
+
+        return (
             <div className='body-container'>
-            <div className='post-body-container'>
-    
-                    <div className='post-headline'>
-                       
-    
-                        <p>Browse.</p>
-                        <p>Share.</p>
-                        <p>Connect.</p>
-    
-                        {/* <img className='transit-icon' src="/images/index_2.jpeg" alt="books" /> */}
+                <div className='post-body-container'>
+                        <div className='post-headline'>
+                            <p>Browse.</p>
+                            <p>Share.</p>
+                            <p>Connect.</p>
+                            {/* <img className='transit-icon' src="/images/index_2.jpeg" alt="books" /> */}
+                        </div> 
+                    <div className='post-headline-1'>
+                    <div className="create-post-index">
+                        <div className="sort-button-container">
+                            <div className="sort-button">Newest</div>
+                        </div>
+                    {/* <div className="sort-button-container">
+                      {this.state.activeButton === "newest" ? 
+                        <div className="sort-button-active" onClick={() => this.setActive("newest")}>Newest</div>
+                      :
+                        <div className="sort-button" onClick={() => this.setActive("newest")}>Newest</div>
+                      }
+                      {this.state.activeButton === "best" ? 
+                        <div className="sort-button-active" onClick={() => this.setActive("best")}>Best</div>
+                      :
+                        <div className="sort-button" onClick={() => this.setActive("best")}>Best</div>
+                      }
+                    </div> */}
+                    <Link className="login-nav-btn" to={'/posts/new'}>Create Post</Link>
+                  </div>
+                        {posts ? 
+                        <ul className='posts-index'>
+                                {posts.map((post)=>( 
+                                    <PostIndexItem
+                                        key={post._id}
+                                        post={post}
+                                        />           
+                                ))}
+                        </ul>
+                        :
+                        null
+                        }
+                    </div>   
             
-                    </div> 
-              
-                <div className='post-headline-1'>
-                    <h2>Newest</h2>
-                    {/* <div>Hello</div> */}
-                    {posts ? 
-                    <ul className='posts-index'>
-                            {posts.map((post)=>(
-                            
-                                <PostIndexItem
-                                    key={post._id}
-                                    post={post}
-                                    />           
-                            ))}
-                    </ul>
-                    :
-                    null
-                    }
-                </div>   
-
-            
-        </div>
-       </div>
-
-    )
+                </div>
+            </div>
+        )
     }
 }
 
