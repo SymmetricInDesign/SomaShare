1607788f26f10a684a15db5afbdecf376b3b0ce2
Donnie Wombough <donniewomboughjunior@gmail.com>
2021-08-05 16:14:43 -0400

    add profile links

--- frontend/src/components/post/post_index_item.jsx
+++ frontend/src/components/post/post_index_item.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {withRouter} from 'react-router';
-import Footer from '../footer/footer'
 
 class PostIndexItem extends React.Component{
 
